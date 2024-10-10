dd83aca99ba2c9a8fbd51d686a7e08504b9708b7
Cypress <kad2321@yahoo.com>
2024-10-09 20:39:01 -0400

    Readability

--- src/main/java/org/launchcode/weapon_inventory/WeaponInventoryApplication.java
+++ src/main/java/org/launchcode/weapon_inventory/WeaponInventoryApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeaponInventoryApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WeaponInventoryApplication.class, args);
+
 	}
 
 }
